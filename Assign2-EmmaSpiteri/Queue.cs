e1111f6ab3f7c90af2743d52a4be863029bf4006
DESKTOP-FU6I8N3\spite <spiteriemma@gmail.com>
2021-04-01 22:49:20 +0200

    Add project files.

---
+++ Assign2-EmmaSpiteri/Queue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assign2_EmmaSpiteri
+{
+    public class Queuet<T>
+    {
+        private List<T> queue = new List<T>();
+
+        public int Size { get { return queue.Count; } }
+
+        public void Enque(T elem)
+        {
+            queue.Insert(0, elem);
+        }
+
+        public T Dequeue()
+        {
+
+            if(Size == 0)
+            {
+                throw new InvalidOperationException("There is no element");
+            }
+            
+            T elementToReturn = queue[Size - 1];
+            queue.RemoveAt(Size - 1);
+
+            return elementToReturn;
+        }
+
+    }
+}
---
+++ Assign2-EmmaSpiteri/Stack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assign2_EmmaSpiteri
+{
+    class Stack<T>
+    {
+        private List<T> elements = new List<T>();
+
+        public int size()
+        {
+            return elements.Count;
+
+        }
+
+        public void Push(T element)
+        {
+            elements.Add(element);
+        }
+
+        public T Pop()
+        {
+            T objectToReturn = elements[elements.Count - 1];
+            elements.RemoveAt(elements.Count - 1);
+
+            return objectToReturn;
+        }
+
+        public T Peek()
+        {
+            T objectToReturn = elements[elements.Count - 1];
+
+            return objectToReturn;
+
+        }
+
+    }
+}
